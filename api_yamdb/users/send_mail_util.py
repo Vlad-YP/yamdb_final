ef86a49c81bb806367ace7f54df3adb2a8087195
Владислав Максимов <vlsdmaximov@yandex.com>
2022-08-07 10:31:38 +0600

    test-action

--- api_yamdb/users/send_mail_util.py
+++ api_yamdb/users/send_mail_util.py
@@ -4,7 +4,7 @@ from django.core.mail import send_mail
 
 def send_confirmation_mail(mail, confirmation_code):
     send_mail(
-        'Код подтверждения для доступа в api yamdb',
+        'Код подтверждения для доступа в api yamdb.',
         f'Ваш код: {confirmation_code}',
         f'{settings.NO_REPLY_EMAIL}',
         [mail],
